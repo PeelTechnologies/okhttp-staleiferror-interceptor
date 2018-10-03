cbb141b443123d51852b678b720334e4d41ae7bb
swaroop <swaroop@peel.com>
2018-10-03 13:48:58 -0700

    added tests for caching and stale response

--- src/main/java/com/peel/okhttp3/staleiferror/StaleIfErrorInterceptor.java
+++ src/main/java/com/peel/okhttp3/staleiferror/StaleIfErrorInterceptor.java
@@ -16,16 +16,43 @@
 package com.peel.okhttp3.staleiferror;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 import okhttp3.CacheControl;
 import okhttp3.Interceptor;
 import okhttp3.Request;
 import okhttp3.Response;
+import okhttp3.CacheControl.Builder;
 
 public class StaleIfErrorInterceptor implements Interceptor {
     // based on code from https://github.com/square/okhttp/issues/1083 by jvincek
+	private static final int DEFULAT_MAX_STALE_DURATION = 28;
+	private static final TimeUnit DEFULAT_MAX_STALE_TIMEUNIT = TimeUnit.DAYS;
+	
+	private final int staleDuration;
+	private final TimeUnit staleDurationTimeUnit;
+	
+    public StaleIfErrorInterceptor(int staleDuration, TimeUnit staleDurationTimeUnit) {
+		super();
+		this.staleDuration = staleDuration;
+		this.staleDurationTimeUnit = staleDurationTimeUnit;
+	}
+    
+    public StaleIfErrorInterceptor() {
+		super();
+		this.staleDuration = -1;
+		this.staleDurationTimeUnit = null;
+	}
+    
+	private int getMaxStaleDuration() {
+		return staleDuration != -1 ? staleDuration : DEFULAT_MAX_STALE_DURATION;
+	}
+	
+	private TimeUnit getMaxStaleDurationTimeUnit() {
+		return staleDurationTimeUnit != null ? staleDurationTimeUnit : DEFULAT_MAX_STALE_TIMEUNIT;
+	}
 
-    @Override
+	@Override
     public Response intercept(Interceptor.Chain chain) throws IOException {
         Response response = null;
         Request request = chain.request();
@@ -43,14 +70,18 @@ public class StaleIfErrorInterceptor implements Interceptor {
             // original request error
         }
 
-        if (response == null || !response.isSuccessful()) {
-            Request newRequest = request.newBuilder().cacheControl(CacheControl.FORCE_CACHE).build();
-            try {
-                response = chain.proceed(newRequest);
-            } catch (Exception e) { // cache not available
-                throw e;
-            }
-        }
+		if (response == null || !response.isSuccessful()) {
+		 CacheControl cacheControl = new Builder()
+				      .onlyIfCached()
+				      .maxStale(getMaxStaleDuration(), getMaxStaleDurationTimeUnit())
+				      .build();
+			Request newRequest = request.newBuilder().cacheControl(cacheControl).build();
+			try {
+				response = chain.proceed(newRequest);
+			} catch (Exception e) { // cache not available
+				throw e;
+			}
+		}
 
         return response;
     }
